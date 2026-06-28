905f0fbf4aa302d1d567bdcbdaa30b4c8105c076
Joshua Cruz <joshua.cruz@tecdesoftware.edu.mx>
2026-06-28 09:06:44 -0600

    feat: inicio de mi API

--- GrindCore.Domain/Interfaces/IAthleteRepository.cs
+++ GrindCore.Domain/Interfaces/IAthleteRepository.cs
@@ -1,6 +1,24 @@
+using GrindCore.Models;
+
 namespace GrindCore.Domain.Interfaces;
 
-public class IAthleteRepository
+public interface IAthleteRepository
 {
-    
+    void DeleteAthlete(int id);
+    IEnumerable<Athlete> GetAll();
+    Athlete? GetById(int id);
+    void AddAthlete(Athlete athlete);
+    void UpdateAthlete(Athlete athlete);
+    void AddLog(int athleteId, WorkoutLog log);
+
+    IEnumerable<UserRoutine> GetRoutinesByAthlete(int athleteId);
+    UserRoutine? GetRoutineById(int id);
+    void AddRoutine(UserRoutine routine);
+    void UpdateExerciseSeries(int exerciseId, int sets, int reps, double weight, double rpe);
+
+    ScheduledSession? GetSessionByDate(int athleteId, DateOnly date);
+    void SaveOrUpdateSession(ScheduledSession session);
+    void DeleteSession(int athleteId, DateOnly date);
+    HashSet<DateOnly> GetScheduledDates(int athleteId);
+    IEnumerable<ScheduledSession> GetSessionsByAthlete(int athleteId);
 }
